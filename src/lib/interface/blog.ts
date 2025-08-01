1c0b70eb44c2544cbcac5f2abf63e94e37e09ba8
Santos2175 <santoshgurung949@gmail.com>
2025-08-02 04:16:45 +0545

    feat(blog): create blog model with mongoose

---
+++ src/lib/interface/blog.ts
@@ -0,0 +1,15 @@
+import { Types } from 'mongoose';
+
+// Represents shape of blog object used during creation
+export interface IBlog {
+  title: string;
+  content: string;
+  author: Types.ObjectId;
+}
+
+// Represents shape of user object used during API response
+export interface IBlogResponse extends IBlog {
+  _id: Types.ObjectId;
+  createdAt: Date;
+  updated: Date;
+}
---
+++ src/models/blog.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { IBlog } from '../lib/interface/blog';
+
+// Blog schema definition
+const blogSchema = new Schema<IBlog>(
+  {
+    title: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Compound indexing for faster searching blogs by certain author
+blogSchema.index({ author: 1, createdAt: -1 });
+
+// Customize JSON serialization: remove unnecessary field
+// '__v' before sending the blog object in API responses
+blogSchema.set('toJSON', {
+  transform: function (_doc, ret: Record<string, any>) {
+    delete ret.__v;
+    return ret;
+  },
+});
+
+// Blog model for communicating to db
+export const Blog = model('Blog', blogSchema);
